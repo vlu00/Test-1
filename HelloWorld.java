ab44a2a69771aa1cf860d1ca11648d102bd4b410
vlu00 <vlu00@stuy.edu>
2018-09-14 19:24:09 -0400

    added new java file

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello my name is Vivien, and my favorite color is blue."); // Display the string.
+    }
+}
